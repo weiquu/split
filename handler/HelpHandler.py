497bff8405ee247592baed11764407de8c439352
Neo Wei Qing <ztolenstar@hotmail.com>
2022-08-15 17:33:33 +0200

    Checking errors

--- handler/HelpHandler.py
+++ handler/HelpHandler.py
@@ -1,11 +1,11 @@
-def help():
-    str = 'Welcome to $plit! Please find a list of commands below' + '\n'
-    str += '/creategroup - Creates a new group of expenses to split' + '\n'
-    str += '/entergroup - Enters a group for you to split expenses' + '\n'
-    str += '/addusers - Adds users to the group you are currently in' + '\n' 
-    str += '/viewusers - Views the users in the group you are currently in' + '\n'
-    str += '/expense - Adds an expense to the group you are currently in' + '\n'
-    str += '/viewall - Views all expenses in the group you are currently in, including those which have been split' + '\n'
-    str += '/outstanding - Views the expenses which have not been split in the group you are currently in' + '\n'
-    str += '/split - Shows you the split (i.e. who pays who how much). To finalise the split and mark the expenses as split, press yes; else, press no' + '\n'
-    return str
+def help(update, context):
+    msg = 'Welcome to $plit! Please find a list of commands below' + '\n'
+    msg += '/creategroup - Creates a new group of expenses to split' + '\n'
+    msg += '/entergroup - Enters a group for you to split expenses' + '\n'
+    msg += '/addusers - Adds users to the group you are currently in' + '\n' 
+    msg += '/viewusers - Views the users in the group you are currently in' + '\n'
+    msg += '/expense - Adds an expense to the group you are currently in' + '\n'
+    msg += '/viewall - Views all expenses in the group you are currently in, including those which have been split' + '\n'
+    msg += '/outstanding - Views the expenses which have not been split in the group you are currently in' + '\n'
+    msg += '/split - Shows you the split (i.e. who pays who how much). To finalise the split and mark the expenses as split, press yes; else, press no' + '\n'
+    update.message.reply_text(msg)
--- util/Utilities.py
+++ util/Utilities.py
@@ -3,5 +3,5 @@ def formatUsernamesFromArray(arr):
     str = ''
     for username in arr:
         str += username + ', '
-    str = str[0, -2]
+    #str = str[0, -2]
     return str
