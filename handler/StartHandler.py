2c2ea7ba6a4e36c667406221b86f5ebad33bdef0
Neo Wei Qing <ztolenstar@hotmail.com>
2022-08-15 22:15:51 +0200

    Edit start message

--- handler/StartHandler.py
+++ handler/StartHandler.py
@@ -9,6 +9,13 @@ def start(update, context):
     userToAdd = UserDTO(chat_id, username)
     success = UserDAO().addUser(userToAdd)
     if (success):
-        update.message.reply_text("Welcome (: Type /help to see a list of commands")
+        msg = "Welcome (: This bot is used to track expenses to be split (or your own personal expenses as well)" + '\n'
+        msg += "You can type /help to see a list of commands" + '\n\n'
+        msg += 'To create a group, use the /creategroup command, and if the group creator has already added you to a group, you can use the /entergroup command' + '\n'
+        msg += 'Within the group, use /expense to create a new expense, then enter the relevant details.' + '\n'
+        msg += 'Note: When you select the users to split an expense between, you yourself are not automatically included into the split. '
+        msg += 'Hence, if you paid your someone else, just select their name (and not your own). If you want to track your own personal expenses, select only your name.\n\n'
+        msg += 'message @weiquu for anyth tqvm'
+        update.message.reply_text(msg)
     else:
         update.message.reply_text("We are unable to register you at this time. Please try again later.")
